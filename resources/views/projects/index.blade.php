f6ac6a01ee3219d2e1ab002249150e560b61afc0
Ismailfaris <42093348+Ismailfaris@users.noreply.github.com>
2022-03-17 12:34:33 +0100

    dz

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Hey</h2>
+                <h2>Heeey</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('projects.create') }}" title="Create a project"> <i class="fas fa-plus-circle"></i>
